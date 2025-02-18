13668ab2980d4eee0501419770df01fcdd54dee9
carlton_a <105845362+carltonajr@users.noreply.github.com>
2025-02-18 18:25:03 -0500

    Update InventoryHome.java

--- InventoryHome.java
+++ InventoryHome.java
@@ -1,6 +1,4 @@
-//import java.util.Scanner;  // Import the Scanner class
 import javax.swing.*;
-
 import java.awt.*;
 import java.awt.event.*;
 
@@ -21,6 +19,7 @@ class oop_inventory {
         JButton clickinventory_add = new JButton("New Inventory Entry");
         clickinventory_add.setBounds(200,10,1,3);
         inventory_home.add(clickinventory_add);
+        int testing_value = 0;
         clickinventory_add.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e) {
                     JPanel new_entry_panel = new JPanel();
@@ -57,29 +56,63 @@ class oop_inventory {
                     public void actionPerformed(ActionEvent e) {
                     inventory_home.setVisible(true);
                     new_entry_panel.setVisible(false);
-                    main_frame.setVisible(true);
                     }
                     });
+                    
+                    System.out.println(testing_value + 1);
                     inventory_home.setVisible(false);
-                    main_frame.setVisible(true);
                     }});
-                JButton clickinventory_update = new JButton("Update Current Inventory");
-                clickinventory_update.setBounds(200,10,1,3);
-                inventory_home.add(clickinventory_update);
+        JButton clickinventory_update = new JButton("Update Current Inventory");
+        clickinventory_update.setBounds(200,10,1,3);
+        inventory_home.add(clickinventory_update);
+        clickinventory_update.addActionListener(new ActionListener() {
+        public void actionPerformed(ActionEvent e) {
+            JPanel update_entry_panel = new JPanel();
+            update_entry_panel.setLayout(new BoxLayout(update_entry_panel, BoxLayout.Y_AXIS));
+            main_frame.add(update_entry_panel);
+            JComboBox<String> dropdown_type = new JComboBox<>(new String[] {"","Mild Steel", "Stainless Steel", "Aluminum","Other"});
+            JComboBox<String> dropdown_finish = new JComboBox<>(new String[] {"Mill", "2B", "Option 3"});
+            JComboBox<String> dropdown_thickness_gauges_inches = new JComboBox<>(new String[] {"20ga", "18ga", "16ga", "14ga", "12ga", "10ga", "8ga", "7ga", "3/16", "1/4", "5/16", "3/8"});
+            JComboBox<String> dropdown_size = new JComboBox<>(new String[] {"48x96", "60x96", "48x120", "60x120"});
+                       
+            int x = (x_screen - main_frame.getWidth()) / 4;
+            int y = (y_screen - main_frame.getWidth()) / 4;
+            update_entry_panel.setSize(x, y);
+            update_entry_panel.add(new JLabel(""));
+            update_entry_panel.add(new JLabel("Metal Type"));
+            update_entry_panel.add(dropdown_type);
+            update_entry_panel.add(new JLabel("Metal Finish"));
+            update_entry_panel.add(dropdown_finish);
+            update_entry_panel.add(dropdown_thickness_gauges_inches);
+            update_entry_panel.add(dropdown_size);
+            update_entry_panel.add(new JLabel("Enter total number of sheets."));
+            update_entry_panel.add(new JTextField("", 1));
 
+            inventory_home.setVisible(false);
+            System.out.println(testing_value + 1);
+            update_entry_panel.setVisible(true);
 
-                JButton clickexit = new JButton("Exit");
-                clickexit.setBounds(200,10,-1,-30);
-                inventory_home.add(clickexit);
-        
-                main_frame.setAutoRequestFocus(true);
-                
-                clickexit.addActionListener(new ActionListener() {
-                    public void actionPerformed(ActionEvent e) {
-                    main_frame.dispose();
+            JButton cancel_entry = new JButton("Cancel Update Entry");
+            cancel_entry.setBounds(0,150,1,30);
+            update_entry_panel.add(cancel_entry);
+                        
+            cancel_entry.addActionListener(new ActionListener() {
+                public void actionPerformed(ActionEvent e) {
+                    inventory_home.setVisible(true);
+                    update_entry_panel.setVisible(false);
+                    System.out.println(testing_value + 1);
                     }
                     });
-        main_frame.setVisible(true);
-        //myObj.close();
+            }});
+
+    JButton clickexit = new JButton("Exit");
+    clickexit.setBounds(200,10,-1,-30);
+    inventory_home.add(clickexit);
+    main_frame.setAutoRequestFocus(true);
+    clickexit.addActionListener(new ActionListener() {
+        public void actionPerformed(ActionEvent e) {
+            main_frame.dispose(); }});
+    System.out.println(testing_value);    
+    main_frame.setVisible(true);
     }
 }

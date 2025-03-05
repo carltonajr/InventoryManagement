102d3d44a9aa88fd6a18302ea865228f6c7ac1fc
carlton_a <105845362+carltonajr@users.noreply.github.com>
2025-03-05 16:54:42 -0500

    Update InventoryHome.java

--- InventoryHome.java
+++ InventoryHome.java
@@ -2,21 +2,12 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
 import java.sql.*;
-import java.util.ArrayList;
-import java.util.Collection;
+import java.sql.Date;
+import java.util.*;
+
 
 class oop_inventory {
-    private String product_id;
-    private Boolean isactive;
-    private Date last_updated;
-    private String metal_type;
-    private float metal_thickness_in;
-    private float metal_thickness_mm;    
-    private String sheet_size_WL;
-    private float total_counts;
-    private String location_area;
-    private String rack;
-    private String notes;
+
     public static <picks> void main(String[] args) {
         JFrame main_frame = new JFrame();
         main_frame.setResizable(false);
@@ -24,7 +15,7 @@ class oop_inventory {
         Dimension screenSize = toolkit.getScreenSize();
         int x_screen = screenSize.width;
         int y_screen = screenSize.height;
-
+        System.out.println();
         main_frame.setTitle("Inventory App - Landing Page");
         main_frame.setSize(x_screen/2,y_screen/2);
         main_frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -51,14 +42,14 @@ class oop_inventory {
         inventory_panel_template.setSize(x_template, y_template);
         inventory_panel_template.add(new JLabel("Metal Type"));
         inventory_panel_template.add(dropdown_type);
-        inventory_panel_template.add(new JLabel("Metal Finish"));
-        inventory_panel_template.add(dropdown_finish);
+        //inventory_panel_template.add(new JLabel("Metal Finish"));
+        //inventory_panel_template.add(dropdown_finish);
         inventory_panel_template.add(new JLabel("Sheet Thickness"));
         inventory_panel_template.add(dropdown_thickness_gauges_inches);
         inventory_panel_template.add(new JLabel("Sheet Size"));
         inventory_panel_template.add(dropdown_size);
-        inventory_panel_template.add(new JLabel("Enter total number of sheets."));
-        inventory_panel_template.add(enter_counts);
+        //inventory_panel_template.add(new JLabel("Enter total number of sheets."));
+        //inventory_panel_template.add(enter_counts);
 
         inventory_panel_template.add(buttons);
 
@@ -66,7 +57,6 @@ class oop_inventory {
         filter_entry.addActionListener(new ActionListener() {
 
             public void actionPerformed(ActionEvent e) {
-
                 Collection<String> items = new ArrayList<>();
                 items.add(dropdown_type.getSelectedItem().toString());
                 items.add(dropdown_finish.getSelectedItem().toString());
@@ -74,11 +64,84 @@ class oop_inventory {
                 items.add(dropdown_size.getSelectedItem().toString());
                 
                 Collection<String> filters = new ArrayList<>();
+                
                 for (String item : items) {
                     if (!"Select Option".equals(item)){
                     filters.add(item);}
                 }
-                System.out.println(filters);
+                try {
+            // Load Type-4 Driver
+            // MySQL Type-4 driver class
+            // Establish connection
+            //
+            // Create a statement
+            String url = "jdbc:sqlite:C:lib/inventory.db";
+            HashMap<String, String> selections_map = new HashMap<>();
+
+            // metal_type, 
+            // Updated query syntax for modern databases
+            String query_select_all = new String("SELECT * FROM company_inventory;");
+            String query_select_filter = new String("");
+            String query_insert = new String("");
+            String query_delete = new String("");
+            Statement conn_statement = DriverManager.getConnection(url).createStatement();
+            
+            // Execute the query
+            int row_count = 0;
+            ResultSet run_connection = conn_statement.executeQuery(query_select_all);
+            Collection<String> product = new ArrayList<>();
+            Collection<Boolean> is_active = new ArrayList<>();
+            Collection<String> last_updates = new ArrayList<>();
+            Collection<Float> metal_thickness_in = new ArrayList<>();
+            Collection<Float> metal_thickness_mm = new ArrayList<>();
+            Collection<String> sheet_size_WL = new ArrayList<>();
+            Collection<Float> counts = new ArrayList<>();
+            Collection<String> location = new ArrayList<>();
+            Collection<String> rack = new ArrayList<>();
+            Collection<String> notes = new ArrayList<>();
+
+            while(run_connection.next()){
+                String product_id = run_connection.getString("product_id");
+                Boolean active = run_connection.getBoolean("is_active");
+                String updated = run_connection.getString("last_updated");
+                Float in_thick = run_connection.getFloat("thickness_in");
+                Float mm_thick = run_connection.getFloat("thickness_mm");
+                String sheet_size = run_connection.getString("sheet_size_WL");
+                Float total_counts = run_connection.getFloat("total_counts");
+                String rack_location = run_connection.getString("location_area");
+                String in_rack = run_connection.getString("rack");
+                String item_notes = run_connection.getString("notes");
+                
+                product.add(product_id);
+                is_active.add(active);
+                last_updates.add(updated);
+                metal_thickness_in.add(in_thick);
+                metal_thickness_mm.add(mm_thick);
+                sheet_size_WL.add(sheet_size);
+                counts.add(total_counts);
+                location.add(rack_location);
+                rack.add(in_rack);
+                notes.add(item_notes);
+                row_count = row_count + 1;
+            }
+            System.out.println(product);
+            System.out.println(last_updates);
+            System.out.println(sheet_size_WL);
+            System.out.println(sheet_size_WL);
+            System.out.println(counts);
+            System.out.println(notes);
+            System.out.println("Number of rows affected by this query: " + row_count);
+
+            // Close the connection
+            conn_statement.close();
+            //System.out.println("Connection closed.");
+        }
+        catch (SQLException error_sql_query) {
+            System.err.println("SQL Error: "
+                               + error_sql_query.getMessage());
+        }
+                
+        System.out.println(filters);
                 JOptionPane.showMessageDialog(inventory_home, ("Filtering with criteria: " + filters));
                 }          
             });

fa1cae28ac71569f0c8cda40879e42f8604baf00
carlton_a <105845362+carltonajr@users.noreply.github.com>
2025-04-02 17:08:44 -0400

    Update InventoryHome.java

--- InventoryHome.java
+++ InventoryHome.java
@@ -1,3 +1,4 @@
+//Imports for SQL, JavaX.Swing, AWT
 import javax.swing.*;
 import javax.swing.table.DefaultTableModel;
 import java.awt.*;
@@ -5,45 +6,54 @@ import java.awt.event.*;
 import java.sql.*;
 import java.util.*;
 
+
 public class oop_inventory {
 
     public static <picks> void main(String[] args) {
+        //create intial JFrame for starting point/ homepage
+        //Do not make it resizeable
+        //Set the size of the window relative to the PC screen size
+        //Set Styling such as Title at the top of the window, define size of window.
+        //Clicking the window closed will end the program.
         JFrame main_frame = new JFrame();
         main_frame.setResizable(false);
         Toolkit toolkit = Toolkit.getDefaultToolkit();
         Dimension screenSize = toolkit.getScreenSize();
         int x_screen = screenSize.width;
         int y_screen = screenSize.height;
-        System.out.println();
         main_frame.setTitle("Inventory App - Landing Page");
         main_frame.setSize(x_screen/2,y_screen/2);
         main_frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        
+        //JPanels to hold user interaction fields. Store in the JFrame created earlier.
         JPanel inventory_home = new JPanel();
         JPanel buttons = new JPanel();
         main_frame.add(inventory_home);
 
+        //Button to open field to add new data 
         JButton clickinventory_add = new JButton("New Inventory Entry");
         clickinventory_add.setBounds(200,10,1,3);
         inventory_home.add(clickinventory_add);
 
-        JPanel inventory_panel_template = new JPanel();
-        int x_template = (x_screen - main_frame.getWidth()) / 8;
-        int y_template = (y_screen - main_frame.getWidth()) / 8;
-        inventory_panel_template.setLayout(new BoxLayout(inventory_panel_template, BoxLayout.Y_AXIS));
-        inventory_panel_template.setSize(x_template, y_template);
+        //Field to update existing inventory
+        JPanel update_inventory_panel_template = new JPanel();
+        int x_template = (x_screen - main_frame.getWidth()) / 4;
+        int y_template = (y_screen - main_frame.getWidth()) / 4;
+        update_inventory_panel_template.setLayout(new BoxLayout(update_inventory_panel_template, BoxLayout.Y_AXIS));
+        update_inventory_panel_template.setSize(x_template, y_template);
         JComboBox<String> dropdown_type = new JComboBox<>(new String[] {"Select Option","Mild Steel", "Stainless Steel", "Aluminum", "430", "Other"});
         JComboBox<String> dropdown_thickness_gauges_inches = new JComboBox<>(new String[] {"Select Option","0.03", "0.04", "0.05", "0.06", "0.07", "0.105", "0.12", "8ga", "0.188", "0.25", "0.3125", "0.375", "0.625", "0.50", "0.75","Other"});
         JComboBox<String> dropdown_size = new JComboBox<>(new String[] {"Select Option", "48x96", "60x96", "48x120", "60x120", "Other"});
         
-        inventory_panel_template.setSize(x_template, y_template);
-        inventory_panel_template.add(new JLabel("Metal Type"));
-        inventory_panel_template.add(dropdown_type);
-        inventory_panel_template.add(new JLabel("Sheet Thickness [inches]"));
-        inventory_panel_template.add(dropdown_thickness_gauges_inches);
-        inventory_panel_template.add(new JLabel("Sheet Size [WxL]"));
-        inventory_panel_template.add(dropdown_size);
+        update_inventory_panel_template.setSize(x_template, y_template);
+        update_inventory_panel_template.add(new JLabel("Metal Type"));
+        update_inventory_panel_template.add(dropdown_type);
+        update_inventory_panel_template.add(new JLabel("Sheet Thickness [inches]"));
+        update_inventory_panel_template.add(dropdown_thickness_gauges_inches);
+        update_inventory_panel_template.add(new JLabel("Sheet Size [WxL]"));
+        update_inventory_panel_template.add(dropdown_size);
 
-        inventory_panel_template.add(buttons);
+        update_inventory_panel_template.add(buttons);
         
 
         JButton filter_entry = new JButton("Filter Selections");
@@ -52,9 +62,8 @@ public class oop_inventory {
         JButton cancel_entry = new JButton("Cancel Entry");
         buttons.add(filter_entry);
         buttons.add(cancel_entry);
-        String query_select_all = new String("SELECT * FROM company_inventory");
-        String query = new String("SELECT * FROM company_inventory WHERE ");
-        StringBuilder query_select_filter = new StringBuilder("");
+        StringBuilder query = new StringBuilder("SELECT * FROM company_inventory WHERE ");
+        StringBuffer query_select_filter = new StringBuffer("");
         filter_entry.addActionListener(new ActionListener() {
             
             JFrame found_inventory_frame = new JFrame();
@@ -64,10 +73,20 @@ public class oop_inventory {
                 found_inventory_frame.setSize(x_screen/4, y_screen/4);
                 
                 HashMap<String, String> selections_map = new HashMap<>();
+                Object type = dropdown_type.getSelectedItem();
+                Object thickness_in = dropdown_thickness_gauges_inches.getSelectedItem();
+                Object sheet_size = dropdown_size.getSelectedItem();
                 
-                selections_map.put("metal_type", dropdown_type.getSelectedItem().toString());
-                selections_map.put("thickness_in", dropdown_thickness_gauges_inches.getSelectedItem().toString());
-                selections_map.put("sheet_size_WL", dropdown_size.getSelectedItem().toString());
+                selections_map.put("metal_type", type.toString());
+                selections_map.put("thickness_in", thickness_in.toString());
+                selections_map.put("sheet_size_WL", sheet_size.toString());
+                
+                //Reset the ComboBoxes [Dropdowns] to the default 
+                dropdown_type.setSelectedItem("Select Option");
+                dropdown_thickness_gauges_inches.setSelectedItem("Select Option");
+                dropdown_size.setSelectedItem("Select Option");
+
+                
                 Set<Map.Entry<String, String>> selections_map_entrySet = selections_map.entrySet();
                 int counting = 0;
                 Collection<String> filters = new ArrayList<>();
@@ -78,7 +97,7 @@ public class oop_inventory {
                     if (!"Select Option".equals(item.getValue())){
                         filters.add(item.getValue().toString());
                         searchingStrings.add(item.getKey() + " = \"" + item.getValue()+"\"");
-                        query_select_filter.append(query + item.getKey() + " = \"" + item.getValue()+"\"");
+                        query_select_filter.append(item.getKey() + " = \"" + item.getValue()+"\"");
                         
                     System.out.println("Filters:" + filters.size());
                     if (filters.size() > 3 && filters.size() > counting){
@@ -87,9 +106,12 @@ public class oop_inventory {
                         }
                     counting += 1;
                     }
+                    
                 }
-                System.out.println(searchingStrings.size());
+                
                 selections_map.clear();
+                System.out.println(query);
+
                 try {
             String[] columnNames = {"Metal ID", "Active Status", "Last Updated", "Metal Type", "Thickness [in.]", "Thickness [MM]", 
                                 "Sheet Size [WxL]", "Total Counts", "Location Area", "Rack"}; // Column headers
@@ -97,11 +119,12 @@ public class oop_inventory {
             
             // Updated query syntax for modern databases
             Statement conn_statement = DriverManager.getConnection("jdbc:sqlite:C:lib/inventory.db").createStatement();
-            
+            query.append(query_select_filter);
             // Execute the query
+
+            //count the rows being created 
             int row_count = 0;
-            System.out.println(query_select_filter);
-            ResultSet run_connection = conn_statement.executeQuery(query_select_filter + ";");
+            ResultSet run_connection = conn_statement.executeQuery(query + ";");
             while(run_connection.next()){
                 Object[] row = {run_connection.getString("product_id"),
                 run_connection.getBoolean("is_active"),
@@ -137,7 +160,7 @@ public class oop_inventory {
         
         clickinventory_add.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e) {
-                    JPanel new_entry_panel = inventory_panel_template;
+                    JPanel new_entry_panel = update_inventory_panel_template;
                     JTextField enter_counts = new JTextField(1);
                     enter_counts.setMaximumSize(screenSize);
                     main_frame.add(new_entry_panel);
@@ -146,19 +169,19 @@ public class oop_inventory {
                     cancel_entry.addActionListener(new ActionListener() {
                     public void actionPerformed(ActionEvent e) {
                     inventory_home.setVisible(true);
-                    inventory_panel_template.setVisible(false);
+                    update_inventory_panel_template.setVisible(false);
                     main_frame.setTitle("Inventory App - Landing Page");
                     }
                     });
                     inventory_home.setVisible(false);
-                    inventory_panel_template.setVisible(true);
+                    update_inventory_panel_template.setVisible(true);
                     }});
         JButton clickinventory_update = new JButton("Update Current Inventory");
         clickinventory_update.setBounds(200,10,1,3);
         inventory_home.add(clickinventory_update);
         clickinventory_update.addActionListener(new ActionListener() {
         public void actionPerformed(ActionEvent e) {
-            JPanel update_entry_panel = inventory_panel_template;
+            JPanel update_entry_panel = update_inventory_panel_template;
             main_frame.add(update_entry_panel);
             inventory_home.setVisible(false);
             update_entry_panel.setVisible(true);

ecd8bac92f73b41c93f8b054b1112fd4d6a40500
carlton_a <105845362+carltonajr@users.noreply.github.com>
2025-04-28 20:57:00 -0400

    Update LoginFrame.java
    Still debugging the query creation and recreation process.

--- LoginFrame.java
+++ LoginFrame.java
@@ -1,97 +1,213 @@
+//Imports for SQL, JavaX.Swing, AWT
 import javax.swing.*;
+import javax.swing.table.DefaultTableModel;
 import java.awt.*;
-import java.awt.event.ActionListener;
-import java.awt.event.ActionEvent;
+import java.awt.event.*;
+import java.sql.*;
+import java.util.*;
 
 
-public class LoginFrame extends JFrame implements ActionListener{
-    public static void main(String[] args){
-        LoginFrame frame = new LoginFrame();
+public class oop_inventory {
+
+    public static <picks> void main(String[] args) {
+        //create intial JFrame for starting point/ homepage
+        //Do not make it resizeable
+        //Set the size of the window relative to the PC screen size
+        //Set Styling such as Title at the top of the window, define size of window.
+        //Clicking the window closed will end the program.
+        JFrame main_frame = new JFrame();
+        main_frame.setResizable(false);
         Toolkit toolkit = Toolkit.getDefaultToolkit();
         Dimension screenSize = toolkit.getScreenSize();
         int x_screen = screenSize.width;
         int y_screen = screenSize.height;
-
-        int x = (x_screen - frame.getWidth()) / 4;  // Center horizontally
-        int y = (y_screen - frame.getWidth()) /4;  // Top of the screen
-        frame.setLocation(x, y);  // Centers the window on the screen
-
-        frame.setSize(250,250); ; // Width: 400 pixels, Height: 300 pixels
-        frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        main_frame.setTitle("Inventory App - Landing Page");
+        main_frame.setSize(x_screen/2,y_screen/2);
+        main_frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        
+        //JPanels to hold user interaction fields. Store in the JFrame created earlier.
         JPanel inventory_home = new JPanel();
-        frame.add(inventory_home);
+        JPanel buttons = new JPanel();
+        main_frame.add(inventory_home);
 
-        GridLayout gridLayout = new GridLayout(6, 1);
+        //Button to open field to add new data 
+        JButton clickinventory_add = new JButton("New Inventory Entry");
+        clickinventory_add.setBounds(200,10,1,3);
+        inventory_home.add(clickinventory_add);
 
-        frame.setContentPane(inventory_home); 
-        inventory_home.setLayout(gridLayout);
-        JLabel userlabel = new JLabel("Username:");
-        inventory_home.add(userlabel);
-        JTextField userinput = new JTextField("", 15);
-        inventory_home.add(userinput);
-        JLabel passlabel = new JLabel("Password:");
-        inventory_home.add(passlabel);
-        JPasswordField passinput = new JPasswordField("", 20);
-
-        passinput.setInputVerifier(new InputVerifier() {
-            @Override
-            public boolean verify(JComponent passinput) {
-                String text = new String(((JPasswordField) passinput).getPassword());
-                if (text.length() <= 25 && text.length() > 0){
-                    JOptionPane.showMessageDialog(frame, "Password must be 30 characters or less.");
-                return true; // Reject invalid input
-        }
-                return false; // Allow valid input
-                }
-            }
-        );
+        //Field to update existing inventory
+        JPanel update_inventory_panel_template = new JPanel();
+        int x_template = (x_screen - main_frame.getWidth()) / 4;
+        int y_template = (y_screen - main_frame.getWidth()) / 4;
+        update_inventory_panel_template.setLayout(new BoxLayout(update_inventory_panel_template, BoxLayout.Y_AXIS));
+        update_inventory_panel_template.setSize(x_template, y_template);
+        JComboBox<String> dropdown_type = new JComboBox<>(new String[] {"Select Option","Mild Steel", "Stainless Steel", "Aluminum", "430", "Other"});
+        JComboBox<String> dropdown_thickness_gauges_inches = new JComboBox<>(new String[] {"Select Option","0.03", "0.04", "0.05", "0.06", "0.07", "0.105", "0.12", "8ga", "0.188", "0.25", "0.3125", "0.375", "0.625", "0.50", "0.75","Other"});
+        JComboBox<String> dropdown_size = new JComboBox<>(new String[] {"Select Option", "48x96", "60x96", "48x120", "60x120", "Other"});
         
-        userlabel.setBounds(50,150,100,30);
-        passlabel.setBounds(50,220,100,30);
-        userinput.setBounds(150,150,150,30);
-        passinput.setBounds(150,220,150,30);
-        //showPassword.setBounds(150,250,150,30);
+        update_inventory_panel_template.setSize(x_template, y_template);
+        update_inventory_panel_template.add(new JLabel("Metal Type"));
+        update_inventory_panel_template.add(dropdown_type);
+        update_inventory_panel_template.add(new JLabel("Sheet Thickness [inches]"));
+        update_inventory_panel_template.add(dropdown_thickness_gauges_inches);
+        update_inventory_panel_template.add(new JLabel("Sheet Size [WxL]"));
+        update_inventory_panel_template.add(dropdown_size);
+
+        update_inventory_panel_template.add(buttons);
         
 
-        inventory_home.add(passinput);
-        JButton clickinventory_home = new JButton("Login");
-        clickinventory_home.setBounds(50,300,100,30);
-        
-        inventory_home.add(clickinventory_home);
-        JButton clickexit = new JButton("Exit");
-        clickexit.setBounds(200,300,100,30);
-        inventory_home.add(clickexit);
-        inventory_home.setMaximumSize(new Dimension(250, 400));
+        JButton filter_entry = new JButton("Filter Selections");
+        buttons.add(Box.createVerticalStrut(50));
 
-
+        JButton cancel_entry = new JButton("Cancel Entry");
+        buttons.add(filter_entry);
+        buttons.add(cancel_entry);
+        StringBuilder query = new StringBuilder("SELECT * FROM company_inventory WHERE ");
+        StringBuffer query_select_filter = new StringBuffer("");
         
-        frame.setMaximizedBounds(new Rectangle(x*2, y, x*2, y));
-        frame.setAlwaysOnTop(true);
-        frame.setAutoRequestFocus(true);
-        
-        clickinventory_home.addActionListener(new ActionListener() {
-        public void actionPerformed(ActionEvent e) {
-        String userinput_string = userinput.getText(); // Retrieve username
-        char[] passinput_string = passinput.getPassword(); // Retrieve password
-        JOptionPane.showMessageDialog(frame, "Username: " + userinput_string + "\nPassword: " + String.valueOf(passinput_string)); // Display inputs
+        JFrame found_inventory_frame = new JFrame();
+        JPanel panel = new JPanel();
+        found_inventory_frame.setSize(x_screen/4, y_screen/4);
 
-            }
-        });
-        clickexit.addActionListener(new ActionListener() {
+        //On Click of the Filter Entry Button
+        filter_entry.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e) {
-            frame.dispose();
+                HashMap<String, String> selections_map = new HashMap<>();
+                Object type = dropdown_type.getSelectedItem();
+                Object thickness_in = dropdown_thickness_gauges_inches.getSelectedItem();
+                Object sheet_size = dropdown_size.getSelectedItem();
+                
+                selections_map.put("metal_type", type.toString());
+                selections_map.put("thickness_in", thickness_in.toString());
+                selections_map.put("sheet_size_WL", sheet_size.toString());
+                
+                //Reset the ComboBoxes [Dropdowns] to the default 
+                dropdown_type.setSelectedItem("Select Option");
+                dropdown_thickness_gauges_inches.setSelectedItem("Select Option");
+                dropdown_size.setSelectedItem("Select Option");
+
+                
+                Set<Map.Entry<String, String>> selections_map_entrySet = selections_map.entrySet();
+                int counting = 0;
+                Collection<String> filters = new ArrayList<>();
+                Collection<String> searchingStrings = new ArrayList<>();
+                
+                for (Map.Entry<String, String> item : selections_map_entrySet)
+                {
+                    if (!"Select Option".equals(item.getValue())){
+                        filters.add(item.getValue().toString());
+                        searchingStrings.add(item.getKey() + " = \"" + item.getValue()+"\"");
+                        query_select_filter.append(item.getKey() + " = \"" + item.getValue()+"\"");
+                        query.append(item.getKey() + " = \"" + item.getValue()+"\"");
+                        if (filters.size() > 1){
+                            query.append("AND");
+                            System.out.println(query);
+                            }
+                    
+                    System.out.println(filters);
+                    System.out.println("\nFilters:" + filters.size());
+                    System.out.println();
+                    }
+                    counting += 1;
+                }
+                // Clearing the selections
+                selections_map.clear();
+                System.out.println("SelectFilter: " + query_select_filter);
+                System.out.println("QueryOnly: " + query);
+
+                try {
+            String[] columnNames = {"Metal ID", "Active Status", "Last Updated", "Metal Type", "Thickness [in.]", "Thickness [MM]", 
+                                "Sheet Size [WxL]", "Total Counts", "Location Area", "Rack"}; // Column headers
+            DefaultTableModel model = new DefaultTableModel(columnNames, 0);
+            
+            // Updated query syntax for modern databases
+            Statement conn_statement = DriverManager.getConnection("jdbc:sqlite:C:lib/inventory.db").createStatement();
+            query.append(query_select_filter);
+            // Execute the query
+
+            //count the rows being created 
+            int row_count = 0;
+            System.out.println(query + ";");
+            ResultSet run_connection = conn_statement.executeQuery(query_select_filter + ";");
+            while(run_connection.next()){
+                Object[] row = {run_connection.getString("product_id"),
+                run_connection.getBoolean("is_active"),
+                run_connection.getString("last_updated"),
+                run_connection.getString("metal_type"),
+                run_connection.getFloat("thickness_in"),
+                run_connection.getFloat("thickness_mm"),
+                run_connection.getString("sheet_size_WL"),
+                run_connection.getFloat("total_counts"),
+                run_connection.getString("location_area"),
+                run_connection.getString("rack"),
+                run_connection.getString("notes")};
+                // The selected data collected in an object and sent to a table 
+                model.addRow(row);
+                row_count = row_count + 1;
             }
+            
+            JScrollPane scrollPane = new JScrollPane(new JTable(model));
+            found_inventory_frame.add(panel.add(scrollPane));
+
+            System.out.println("Number of rows affected by this query: " + row_count);
+
+            // Close the connection
+            conn_statement.close();
+            System.out.println("Connection closed.");
+            found_inventory_frame.setVisible(true);
+        }
+        catch (SQLException error_sql_query) {
+            System.err.println("SQL Error: " + error_sql_query.getMessage());
+        }
+                }          
             });
+        
+        clickinventory_add.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                    JPanel new_entry_panel = update_inventory_panel_template;
+                    JTextField enter_counts = new JTextField(1);
+                    enter_counts.setMaximumSize(screenSize);
+                    main_frame.add(new_entry_panel);
+                    main_frame.setTitle("Inventory App - New Inventory");  
+                        
+                    cancel_entry.addActionListener(new ActionListener() {
+                    public void actionPerformed(ActionEvent e) {
+                    inventory_home.setVisible(true);
+                    update_inventory_panel_template.setVisible(false);
+                    main_frame.setTitle("Inventory App - Landing Page");
+                    }
+                    });
+                    inventory_home.setVisible(false);
+                    update_inventory_panel_template.setVisible(true);
+                    }});
+        JButton clickinventory_update = new JButton("Update Current Inventory");
+        clickinventory_update.setBounds(200,10,1,3);
+        inventory_home.add(clickinventory_update);
+        clickinventory_update.addActionListener(new ActionListener() {
+        public void actionPerformed(ActionEvent e) {
+            JPanel update_entry_panel = update_inventory_panel_template;
+            main_frame.add(update_entry_panel);
+            inventory_home.setVisible(false);
+            update_entry_panel.setVisible(true);
+            main_frame.setTitle("Inventory App - Update Current Inventory");  
 
-        frame.setVisible(true);
-    }
-    
-    //Overriding actionPerformed() method
-    @Override
-    public void actionPerformed(ActionEvent e) {
- 
-    }
-//
+            cancel_entry.addActionListener(new ActionListener() {
+                public void actionPerformed(ActionEvent e) {
+                    inventory_home.setVisible(true);
+                    update_entry_panel.setVisible(false);
+                    main_frame.setTitle("Inventory App - Landing Page");
+                    }
+                    });
+            }});
 
+    JButton clickexit = new JButton("Exit");
+    clickexit.setBounds(200,10,-1,-30);
+    inventory_home.add(clickexit);
+    main_frame.setAutoRequestFocus(true);
+    clickexit.addActionListener(new ActionListener() {
+        public void actionPerformed(ActionEvent e) {
+            main_frame  .dispose(); }});  
+    main_frame.setVisible(true);
+
+    }
 }
